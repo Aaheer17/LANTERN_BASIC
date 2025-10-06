4fcf37c4231a1f5bd0a238ef32ef15295563322f
Aaheer17 <farzanaahmad.17@gmail.com>
2025-10-06 07:41:05 -0400

    Add LANTERN_BASIC shell scripts

---
+++ sbatch_CUDA.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##SBATCH -N 1
+#SBATCH --cpus-per-task=8  
+#SBATCH -n 1
+#SBATCH --job-name=D2_Diff
+##SBATCH --exclusive
+#SBATCH -t 72:00:00
+#SBATCH --mem=150000
+#SBATCH -p bii-gpu
+#SBATCH --gres=gpu:1
+#SBATCH -A bii_nssac
+module load miniforge
+module load texlive
+source activate torch_gpu_renew
+# export RANK=0
+# export WORLD_SIZE=4
+# export MASTER_ADDR="localhost"
+# export MASTER_PORT=12345
+
+export PATH=~/bin:$PATH
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+which dvipng
+python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_shape_model_diffusion_p3_sigmoid.yaml --use_cuda
+#srun python3 main_multiGPU.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_shape_model_Autodiffusion.yaml --use_cuda
+#python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_energy_model_submission.yaml --use_cuda
+#python3 cuda_mem_test.py
---
+++ sbatch_CUDA1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##SBATCH -N 1
+#SBATCH --cpus-per-task=8  
+#SBATCH -n 1
+#SBATCH --job-name=D2_diffP1
+##SBATCH --exclusive
+#SBATCH -t 72:00:00
+#SBATCH --mem=100000
+#SBATCH -p bii-gpu
+#SBATCH --gres=gpu:4
+#SBATCH -A bii_nssac
+module load miniforge
+module load texlive
+
+source activate torch_gpu_renew
+export PATH=~/bin:$PATH
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+which dvipng
+#python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_energy_model_submission.yaml --use_cuda
+python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_shape_model_diffusion_p3_cosine.yaml --use_cuda
+#python3 cuda_mem_test.py
