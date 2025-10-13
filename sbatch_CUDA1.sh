be39d5ec211195fa20221deff145f253af43451c
Aaheer17 <farzanaahmad.17@gmail.com>
2025-10-13 09:07:29 -0400

    Update sbatch_CUDA1.sh

--- sbatch_CUDA1.sh
+++ sbatch_CUDA1.sh
@@ -17,5 +17,5 @@ export PATH=~/bin:$PATH
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 which dvipng
 #python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_energy_model_submission.yaml --use_cuda
-python3 main.py /project/biocomplexity/fa7sa/calo_dreamer/configs/d2_shape_model_diffusion_p3_cosine.yaml --use_cuda
+python3 main.py ./configs/d2_shape_model_diffusion_p3_cosine.yaml --use_cuda
 #python3 cuda_mem_test.py
